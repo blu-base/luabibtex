82c854ca89ff7b333b51d641ba0996baefc32fb3
Eric <echiesse@gmail.com>
2011-11-03 01:21:07 -0300

    Adicionadas funções de formatação e criada generalização para trabalhar com strings os tabelas de strings.

--- src/bibFunctions.lua
+++ src/bibFunctions.lua
@@ -2,9 +2,23 @@ require"luno/funcional"
 require"luno/stringEx"
 require"luno/tableEx"
 
+local function generalize(f, arg)
+    local ret
+    if type(arg) == "string" then
+        ret = f(arg)
+    elseif type(arg) == "table" then
+        ret = F.map(f, arg)
+    else
+        error("bad argument #2 to 'generalize' (string or table expected, got " .. type(arg), 2)
+    end
+
+    return ret
+end
+
 
 function joinAuthors(authorsTable)
-    return stringEx.join(authorsTable, ", ")
+    local authorNames = F.map(stringEx.joinWords, authorsTable)
+    return stringEx.join(authorNames, ", ")
 end
 
 
@@ -15,6 +29,7 @@ function splitName(name)
         table.insert(groups, val)
     end
     name = string.gsub(name, "{.+}", "#g")
+
     local list = stringEx.split(name, "%s")
 
     if not tableEx.isEmpty(groups) then
@@ -36,10 +51,10 @@ function getLastName(name)
 end
 
 
-function putLastNameFirst(name)
-    local list = splitName(name)
+function putLastNameFirst(tbName)
+    local list = copy(tbName)
     table.insert(list, 1, table.remove(list))
-    return stringEx.join(list, " ")
+    return list
 end
 
 
@@ -47,26 +62,48 @@ function encloseParentheses(text)
     return "(" .. text .. ")"
 end
 
-function abbreviateFirstNames(name)
-    local list = splitName(name)
+
+function abbreviateFirstNames(tbName)
+    local list = copy(tbName)
     for i = 1, #list-1 do
         list[i] = string.sub(list[i], 1, 1) .. "."
     end
-    return stringEx.join(list, " ")
+    return list
 end
 
 
 function formatItalics(text)
-    return "\\emph{" .. text .. "}"
+    local f = function(x) return "\\emph{" .. x .. "}" end
+    return generalize(f, text)
 end
 
 
 function formatBold(text)
-    return "\\textbf{" .. text .. "}"
+    local f = function(x) return "\\textbf{" .. x .. "}" end
+    return generalize(f, text)
 end
 
---function main()
---    local name = ""
---    print(F.pipe(abbreviateFirstNames, putLastNameFirst, string.upper)(name))
---end
---main()
+
+function upperCase(arg)
+    return generalize(string.upper, arg)
+end
+
+
+function prepend(str)
+    return function(text)
+        return str .. text
+    end
+end
+
+function append(str)
+    return function(text)
+        return text .. str
+    end
+end
+
+-- Considerar colocar esta função em outro arquivo: --<<<<<
+function nextChar(ch)
+    return string.char(string.byte(ch) + 1)
+end
+
+
